205f75eb6a157c488a861b367a6cb14b23dd199d
edoardo.giacopuzzi <edoardo.giacopuzzi@fht.org>
2023-10-04 16:41:06 +0200

    update nimble and compile

--- local_compile.sh
+++ local_compile.sh
@@ -1,5 +1,5 @@
 nimble build -d:release -d:openmp -d:blas=openblas -d:lapack=openblas
 mv quick_classify bin/
 
-nimble build -d:release -d:openmp -d:blas=openblas -d:lapack=openblas -d:avx512
-mv quick_classify bin/quick_classify_avx512
+#nimble build -d:release -d:openmp -d:blas=openblas -d:lapack=openblas -d:avx512
+#mv quick_classify bin/quick_classify_avx512
